d4320ac1ba92fa98c482db0b30a604d680800725
David Mosher <davidmosher@gmail.com>
2023-08-19 10:18:48 -0400

    basic search results rendering, no active state tracking

--- main.go
+++ main.go
@@ -122,20 +122,21 @@ func (app *App) search(w http.ResponseWriter, r *http.Request) {
 	if result.Error != nil {
 		log.Println(result.Error)
 	}
-	ctx := make(map[string]interface{})
-	debug, err := json.MarshalIndent(securities, "", " ")
-	if err != nil {
-		panic(err.Error())
-	}
-	ctx["securities"] = securities
-	ctx["debug"] = string(debug)
 
-	content, err := handlebars.Render(loadTemplateFile("index.html"), ctx)
-	if err != nil {
-		panic(err.Error())
+	var out string
+	for _, security := range securities {
+		out += fmt.Sprintf(`
+			<li class="group flex cursor-default select-none items-center rounded-md px-3 py-2">
+				<svg class="h-6 w-6 flex-none text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+					stroke="currentColor" aria-hidden="true">
+					<path stroke-linecap="round" stroke-linejoin="round"
+						d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
+				</svg>
+				<span class="ml-3 flex-auto truncate">%s</span>
+			</li>
+		`, security.Name)
 	}
-
-	w.Write([]byte(content))
+	w.Write([]byte(out))
 }
 
 func main() {
