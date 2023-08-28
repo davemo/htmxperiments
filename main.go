d8cff116fe8d7b8154d499a0c5b745d858e3a092
David Mosher <davidmosher@gmail.com>
2023-08-28 10:50:07 -0400

    use fprintf c/o Dan the Man

--- main.go
+++ main.go
@@ -123,9 +123,8 @@ func (app *App) search(w http.ResponseWriter, r *http.Request) {
 		log.Println(result.Error)
 	}
 
-	var out string
 	for _, security := range securities {
-		out += fmt.Sprintf(`
+		fmt.Fprintf(w, `
 			<li class="group flex cursor-default select-none items-center rounded-md px-3 py-2">
 				<svg class="h-6 w-6 flex-none text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
 					stroke="currentColor" aria-hidden="true">
@@ -137,10 +136,8 @@ func (app *App) search(w http.ResponseWriter, r *http.Request) {
 		`, security.Name)
 	}
 	if len(securities) == 0 {
-		out = `<div class="px-6 py-14 text-center sm:px-14"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="mx-auto h-6 w-6 text-gray-900 text-opacity-40"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"></path></svg><p class="mt-4 text-sm text-gray-900">We couldn't find any results that matched your search.</p></div>`
+		fmt.Fprintf(w, `<div class="px-6 py-14 text-center sm:px-14"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="mx-auto h-6 w-6 text-gray-900 text-opacity-40"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"></path></svg><p class="mt-4 text-sm text-gray-900">We couldn't find any results that matched your search.</p></div>`)
 	}
-
-	w.Write([]byte(out))
 }
 
 func main() {
